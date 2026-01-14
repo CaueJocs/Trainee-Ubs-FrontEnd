1da29aea4c3c2a645ee0d336a6c50d18bda3a3ac
Dauboau <romanzinidaniel@usp.br>
2026-01-14 09:29:33 -0300

    Expense Interfaces

--- src/interfaces/Expense.ts
+++ src/interfaces/Expense.ts
@@ -2,73 +2,72 @@ import type { ExpenseCategory } from "@/enums/ExpenseCategory";
 import type { ExpenseStatus } from "@/enums/ExpenseStatus";
 import type { CurrencyCode } from "@/enums/CurrencyCode";
 import type { DecisionType } from "@/enums/DecisionType";
+import type { DepartmentDetailedResponse } from "./Department";
 
 export interface ExpenseRequest {
-  description: string;
-  amount: number;
-  currency: CurrencyCode;
-  category: ExpenseCategory;
-  expenseDate: string; // ISO date string (OffsetDateTime)
-  receiptImage: File;
+    description: string;
+    amount: number;
+    currency: CurrencyCode;
+    category: ExpenseCategory;
+    expenseDate: string; // ISO date string (OffsetDateTime)
+    receiptImage: File;
 }
 
-export interface ExpenseResponse {
-  id: string;
-  employeeId: string;
-  departmentName: string;
-  date: string; // ISO date string (OffsetDateTime)
-  category: ExpenseCategory;
-  amount: number;
-  currency: CurrencyCode;
-  exchangeRate: number;
-  description: string;
-  receiptUrl: string;
-  revision: boolean;
-  createdAt: string; // ISO date string (Instant)
-  updatedAt: string; // ISO date string (Instant)
-  status: ExpenseStatus;
+// Expense Response
+export interface ExpenseResponseBase {
+    id: string;
+    date: string; // ISO date string (OffsetDateTime)
+    category: ExpenseCategory;
+    amount: number;
+    currency: CurrencyCode;
+    exchangeRate: number;
+    description: string;
+    receiptUrl: string;
+    revision: boolean;
+    createdAt: string; // ISO date string (Instant)
+    updatedAt: string; // ISO date string (Instant)
+    status: ExpenseStatus;
+}
+export interface ExpenseResponse extends ExpenseResponseBase {
+    employeeId: string;
+    departmentName: string;
+    managerDecision: ManagerDecisionInfo | null;
+    financeDecision: FinanceDecisionInfo | null;
+}
+export interface ExpenseDetailResponse extends ExpenseResponseBase {
+    employee: EmployeeInfo;
+    department: DepartmentDetailedResponse;
+    managerDecision: ManagerDetailedDecisionInfo | null;
+    financeDecision: FinanceDetailedDecisionInfo | null;
 }
 
 export interface EmployeeInfo {
-  id: string;
-  name: string;
-  email: string;
-  position: string;
+    id: string;
+    name: string;
+    email: string;
+    position: string;
 }
 
-export interface ManagerDecisionInfo {
-  manager: EmployeeInfo;
-  decision: DecisionType;
-  decisionDate: string; // ISO date string (OffsetDateTime)
+// Manager Decision
+export interface ManagerDecisionInfoBase {
+    decision: DecisionType;
+    decisionDate: string; // ISO date string (OffsetDateTime)
+}
+export interface ManagerDetailedDecisionInfo extends ManagerDecisionInfoBase {
+    manager: EmployeeInfo;
+}
+export interface ManagerDecisionInfo extends ManagerDecisionInfoBase {
+    managerId: string;
 }
 
-export interface FinanceDecisionInfo {
-  finance: EmployeeInfo;
-  decision: DecisionType;
-  decisionDate: string; // ISO date string (OffsetDateTime)
+// Finance Decision
+export interface FinanceDecisionInfoBase {
+    decision: DecisionType;
+    decisionDate: string; // ISO date string (OffsetDateTime)
 }
-
-export interface DepartmentDetailedResponse {
-  name: string;
-  currency: CurrencyCode;
-  monthlyBudget: number;
+export interface FinanceDetailedDecisionInfo extends FinanceDecisionInfoBase {
+    finance: EmployeeInfo;
 }
-
-export interface ExpenseDetailResponse {
-  id: string;
-  employee: EmployeeInfo | null;
-  department: DepartmentDetailedResponse | null;
-  date: string; // ISO date string (OffsetDateTime)
-  category: ExpenseCategory;
-  amount: number;
-  currency: CurrencyCode;
-  exchangeRate: number;
-  description: string;
-  receiptUrl: string;
-  managerDecision: ManagerDecisionInfo | null;
-  financeDecision: FinanceDecisionInfo | null;
-  revision: boolean;
-  createdAt: string; // ISO date string (Instant)
-  updatedAt: string; // ISO date string (Instant)
-  status: ExpenseStatus;
+export interface FinanceDecisionInfo extends FinanceDecisionInfoBase {
+    financeId: string;
 }
